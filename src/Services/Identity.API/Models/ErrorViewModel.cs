55f0c530de54821b48b64b8e05e44358404dbecc
Rajesh Kannan <rrajeshkannan@gmail.com>
2020-11-11 17:03:54 +0530

    Identity Microservice

---
+++ src/Services/Identity.API/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GrillFreshInc.Services.Identity.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
